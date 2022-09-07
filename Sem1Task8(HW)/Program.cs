2862f5c63ccc04f288f01cbc2e55c685c9d9fa68
ruslan <rmakovkin2@mail.com>
2022-09-08 02:05:13 +0300

    add task 8 Homework

---
+++ Sem1Task8(HW)/Program.cs
@@ -0,0 +1,23 @@
+﻿//=============================##008##=================================
+// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+//=====================================================================
+
+Console.WriteLine("Программа показывает все чётные числа от 1 до N");  // Описание предназначения программы.
+Console.Write("Введите число N: ");                     // Инструкция для действия. 
+string? inputLineN = Console.ReadLine();
+
+if (inputLineN != null)
+{
+    int inputNumberN = int.Parse(inputLineN);
+    if (inputNumberN > 0)
+    {
+        int startNumber = 2;
+        while (startNumber <= inputNumberN - 2)
+        {
+            Console.Write(startNumber + ", ");
+            startNumber += 2;
+        }
+        Console.Write(inputNumberN-1);
+    }
+}
