8cbd7c3e5e5d6300e18a9ec04f3375a1f4dd3385
ruslan <rmakovkin2@mail.com>
2022-09-07 19:20:19 +0300

    Add 2 task programm

---
+++ Sem1Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿//=============================##001##=================================
+//Напишите программу, которая на вход принимает два числа
+//и проверяет, является ли первое число квадратом второго.
+//=====================================================================
+
+string? inputLineA = Console.ReadLine();
+string? inputLineB = Console.ReadLine();
+
+if (inputLineA != null && inputLineB != null)
+{
+    int inputNumberA = int.Parse(inputLineA);
+    int inputNumberB = int.Parse(inputLineB);
+
+    bool outResult = (inputNumberA * inputNumberA == inputNumberB);
+
+    Console.WriteLine(outResult);
+}
