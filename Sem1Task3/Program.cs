535a70710fec470ab7aac88d1b1851b02d0f9d25
ruslan <rmakovkin2@mail.com>
2022-09-07 20:00:48 +0300

    add new changes

--- Sem1Task3/Program.cs
+++ Sem1Task3/Program.cs
@@ -7,7 +7,7 @@ string? inputLine = Console.ReadLine();
 if (inputLine != null)
 {
     int inputDayOfWeek = int.Parse(inputLine);
-//     string[] dayOfWeek = new string[7];
+//    string[] dayOfWeek = new string[7];
 //     dayOfWeek[0] = "Понедельник";
 //     dayOfWeek[1] = "Вторник";
 //     dayOfWeek[2] = "Среда";
